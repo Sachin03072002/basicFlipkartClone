5052c5f067845c0780dc358202df9290b0375023
Sachin <sachinmondal720@gmail.com>
2022-12-08 23:07:02 +0530

    added post creation

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,15 @@
+const Post=require('../models/post');
+
+
+
+
 module.exports.home = function(req,res){
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title:'Flipkart'
+    // res.cookie('user_id',25);
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:'Flipkart',
+            posts: posts
+        });
+        
     });
 }
